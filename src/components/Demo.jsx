c8b4037cef8b631461560797d5b3a5db8b3b44a2
Papsanly <papsanlysenko@gmail.com>
2023-09-04 12:30:57 +0300

    added history

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -7,8 +7,19 @@ export default function Demo() {
     url: '',
     summary: '',
   })
+  const [allArticles, setAllArticles] = useState([])
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem('articles'),
+    )
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage)
+    }
+  }, [])
 
   const handleSubmit = async e => {
     e.preventDefault()
@@ -16,7 +27,12 @@ export default function Demo() {
 
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary }
+      const updatedAllArticles = [newArticle, ...allArticles]
+
       setArticle(newArticle)
+      setAllArticles(updatedAllArticles)
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
+
       console.log(newArticle)
     }
   }
@@ -50,6 +66,30 @@ export default function Demo() {
             ↵
           </button>
         </form>
+        <div className={'flex flex-col gap-1 max-h-60 overflow-y-auto'}>
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className={'link_card'}
+            >
+              <div className={'copy_btn'}>
+                <img
+                  src={copy}
+                  alt={'copy'}
+                  className={'w-[40%] h-[40%] object-contain'}
+                />
+              </div>
+              <p
+                className={
+                  'flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'
+                }
+              >
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )
